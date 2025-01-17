4f28dd8d587df16b51e3d115070e53652428ab5e
tuananh31j <tuananh31j@gmail.com>
2025-01-17 15:43:11 +0700

    heading field

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
 import {
   AppShell,
@@ -10,6 +11,11 @@ import {
   NumberInput,
   Divider,
   SimpleGrid,
+  Group,
+  ActionIcon,
+  Box,
+  Text,
+  TagsInput,
 } from "@mantine/core";
 import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteSchema, defaultBlockSpecs } from "@blocknote/core";
@@ -17,24 +23,37 @@ import { BlockNoteView } from "@blocknote/mantine";
 import { SideMenuController } from "@blocknote/react";
 import "@blocknote/mantine/style.css";
 import "./App.css";
+import { useDocumentTitle, useScrollIntoView } from "@mantine/hooks";
+import { IconTrash } from "@tabler/icons-react";
+import { useForm } from "@mantine/form";
 
 const apiUrl = import.meta.env.VITE_API_URL;
-
+const initValues = {
+  sections: [],
+  title: "",
+  language: "tiếng Việt",
+  tone: "Professional and informative",
+  word_count: 1500,
+  keywords: [],
+};
 export default function App() {
-  const [title, setTitle] = useState("");
-  const [language, setLanguage] = useState("tiếng Việt");
-  const [tone, setTone] = useState("Professional and informative");
-  const [wordCount, setWordCount] = useState<number>(1500);
-  const [keywords, setKeywords] = useState("");
-  const [loading, setLoading] = useState(false);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [blocks, setBlocks] = useState<any>();
-  const { paragraph, heading } = defaultBlockSpecs;
-  const schema = BlockNoteSchema.create({
-    blockSpecs: {
-      paragraph,
-      heading,
+  useDocumentTitle("AI Editor");
+  const { scrollIntoView, targetRef } = useScrollIntoView<HTMLDivElement>({
+    offset: 60,
+  });
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: initValues,
+    validate: {
+      title: (value) => (value != "" ? null : "Title is require"),
     },
+  });
+  const [loading, setLoading] = useState(false);
+
+  const [blocks, setBlocks] = useState<any>();
+
+  const schema = BlockNoteSchema.create({
+    blockSpecs: defaultBlockSpecs,
   });
 
   const editor = useCreateBlockNote({
@@ -42,34 +61,39 @@ export default function App() {
     initialContent: blocks,
   });
 
-  const handleSubmit = async () => {
+  const fields =
+    form.getValues().sections.length > 0
+      ? form.getValues().sections.map((item, index) => (
+          <Group key={item} mt="xs">
+            <TextInput
+              placeholder="John Doe"
+              withAsterisk
+              style={{ flex: 1 }}
+              key={form.key(`sections.${index}.name`)}
+              {...form.getInputProps(`sections.${index}.name`)}
+            />
+            <ActionIcon
+              color="red"
+              onClick={() => form.removeListItem("sections", index)}
+            >
+              <IconTrash size={16} />
+            </ActionIcon>
+          </Group>
+        ))
+      : [];
+  const handleReset = () => {
+    form.resetDirty();
+    form.setValues(initValues);
+    setBlocks([]);
+  };
+
+  const handleSubmit = async (values: any) => {
     setLoading(true);
-
-    const keywordsArray = keywords.split(",").map((keyword) => keyword.trim());
-
-    const body: {
-      title: string;
-      sections: string[];
-      keywords: string[];
-      tone: string;
-      word_count: number;
-      language: string;
-    } = {
-      title: title,
-      sections: [],
-      keywords: keywordsArray,
-      tone,
-      word_count: wordCount,
-      language,
+    console.log(values);
+    const body = {
+      ...values,
+      sections: values.sections.map((item: any) => item.name),
     };
-    const headings: string[] = editor.document
-      .map((block) => {
-        if (block.content.length > 0 && block.content[0].type === "text") {
-          return block.content[0].text;
-        }
-      })
-      .filter((heading) => heading) as string[];
-    body.sections = headings;
 
     try {
       const response = await fetch(apiUrl, {
@@ -81,10 +105,8 @@ export default function App() {
       });
 
       const data = await response.json();
-      const blocksFromMarkdown = await editor.tryParseMarkdownToBlocks(
-        data.content
-      );
-      setBlocks(blocksFromMarkdown);
+
+      setBlocks(data.content);
     } catch (error) {
       setLoading(false);
       console.error("Error:", error);
@@ -94,40 +116,65 @@ export default function App() {
   };
   useEffect(() => {
     async function loadToHTML() {
-      editor.replaceBlocks(editor.document, blocks);
+      const blocksFromMarkdown = await editor.tryParseMarkdownToBlocks(blocks);
+      await editor.replaceBlocks(editor.document, blocksFromMarkdown);
     }
     loadToHTML();
-  }, [blocks, editor]);
+  });
 
   return (
     <AppShell>
       <AppShell.Main>
         <Container size="xl">
-          <div>
+          <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
             <TextInput
               placeholder="Title..."
-              value={title}
               label="Title"
               withAsterisk
               size={"xl"}
-              onChange={(e) => setTitle(e.target.value)}
+              key={form.key("title")}
+              {...form.getInputProps("title")}
             />
+            <Box>
+              {fields.length > 0 ? (
+                <Group my="xs">
+                  <Text fw={500} size={"md"} style={{ flex: 1 }}>
+                    Heading
+                  </Text>
+                </Group>
+              ) : (
+                ""
+              )}
+
+              {fields}
+
+              <Group mt="md">
+                <Button
+                  onClick={() =>
+                    form.insertListItem("sections", {
+                      name: "",
+                    })
+                  }
+                >
+                  Add heading
+                </Button>
+              </Group>
+            </Box>
             <SimpleGrid cols={2} spacing="md">
               <Stack>
                 <TextInput
                   placeholder="Tone...(default: Professional and informative)"
-                  value={tone}
                   label="Tone"
                   size={"md"}
-                  onChange={(e) => setTone(e.target.value)}
+                  key={form.key("tone")}
+                  {...form.getInputProps("tone")}
                 />
                 <NumberInput
                   label="Word Count"
                   placeholder="word count...(default: 1500)"
-                  value={wordCount}
-                  // variant="unstyled"
                   size={"md"}
-                  onChange={(e) => setWordCount(Number(e))}
+                  key={form.key("word_count")}
+                  {...form.getInputProps("word_count")}
                 />
               </Stack>
               <Stack>
@@ -136,76 +183,112 @@ export default function App() {
                   size={"md"}
                   label="Language"
                   placeholder="Language..."
-                  value={language}
-                  onChange={(e) => setLanguage(e.target.value)}
+                  key={form.key("language")}
+                  {...form.getInputProps("language")}
                 />
-                <TextInput
-                  size={"md"}
-                  withAsterisk
+                <TagsInput
                   label="Keywords"
-                  placeholder="Keywords..."
-                  value={keywords}
-                  onChange={(e) => setKeywords(e.target.value)}
+                  placeholder="Enter keywords..."
+                  size={"md"}
+                  key={form.key("keywords")}
+                  {...form.getInputProps("keywords")}
                 />
               </Stack>
             </SimpleGrid>
-            <Button
-              onClick={handleSubmit}
+            <div
               style={{
-                background:
-                  title && keywords
-                    ? "linear-gradient(45deg, #ff007f, #7f00ff, #00c9ff)"
-                    : "gray",
-                color: "#fff",
-                fontWeight: "bold",
-                borderRadius: "12px",
-                padding: "12px 24px",
-                border: "2px solid transparent",
                 position: "fixed",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "20px",
                 bottom: "20px",
                 right: "20px",
-                overflow: "hidden",
-                boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)",
-                textTransform: "uppercase",
-                opacity: title && keywords ? 1 : 0.5,
               }}
-              disabled={loading || !title || !keywords}
             >
-              {loading ? <Loader color={"white"} size="sm" /> : "AI Generate"}
-              {loading || !title || !keywords ? null : (
-                <div className="shine" />
-              )}
-            </Button>
-          </div>
+              <Button
+                onClick={handleReset}
+                variant="outline"
+                style={{
+                  fontWeight: "bold",
+                  borderRadius: "12px",
+                  padding: "12px 24px",
+                  overflow: "hidden",
+                  textTransform: "uppercase",
+                }}
+              >
+                Reset
+              </Button>
+              <Button
+                onClick={() =>
+                  scrollIntoView({
+                    alignment: "start",
+                  })
+                }
+                type="submit"
+                style={{
+                  background: form.isValid()
+                    ? "linear-gradient(45deg, #ff007f, #7f00ff, #00c9ff)"
+                    : "gray",
+                  color: "#fff",
+                  fontWeight: "bold",
+                  borderRadius: "12px",
+                  padding: "12px 24px",
+                  border: "2px solid transparent",
+
+                  overflow: "hidden",
+                  boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)",
+                  textTransform: "uppercase",
+                  opacity: form.isValid() ? 1 : 0.5,
+                }}
+                disabled={loading || !form.isValid()}
+              >
+                {loading ? <Loader color={"white"} size="sm" /> : "AI Generate"}
+                {loading || !form.isValid() ? null : <div className="shine" />}
+              </Button>
+            </div>
+          </form>
           <Divider orientation="horizontal" my={"xl"} />
 
-          {loading ? (
-            <Stack gap="md">
-              <Skeleton c={"red"} height={40} mt={6} width="50%" radius="xl" />
-              <Skeleton height={30} mt={6} width="20%" radius="xl" />
-              <Skeleton height={30} mt={6} width="30%" radius="xl" />
-              <Skeleton height={20} mt={6} width="80%" radius="xl" />
-              <Skeleton height={30} mt={6} width="40%" radius="xl" />
-              <Skeleton height={30} mt={6} width="70%" radius="xl" />
-              <Skeleton height={30} mt={6} width="90%" radius="xl" />
-              <Skeleton height={30} mt={6} width="60%" radius="xl" />
-              <Skeleton height={30} mt={6} width="30%" radius="xl" />
-              <Skeleton height={30} mt={6} width="70%" radius="xl" />
-              <Skeleton height={30} mt={6} width="80%" radius="xl" />
-              <Skeleton height={30} mt={6} width="80%" radius="xl" />
-              <Skeleton height={30} mt={6} width="40%" radius="xl" />
-              <Skeleton height={30} mt={6} width="50%" radius="xl" />
-              <Skeleton height={30} mt={6} width="77%" radius="xl" />
-            </Stack>
-          ) : (
-            <BlockNoteView editor={editor} theme={"light"} sideMenu={false}>
-              <div
-                style={{ position: "relative", display: "flex", zIndex: 1000 }}
-              >
-                <SideMenuController />
-              </div>
-            </BlockNoteView>
-          )}
+          <div ref={targetRef}>
+            {loading ? (
+              <Stack gap="md">
+                <Skeleton
+                  c={"red"}
+                  height={40}
+                  mt={6}
+                  width="50%"
+                  radius="xl"
+                />
+                <Skeleton height={30} mt={6} width="20%" radius="xl" />
+                <Skeleton height={30} mt={6} width="30%" radius="xl" />
+                <Skeleton height={20} mt={6} width="80%" radius="xl" />
+                <Skeleton height={30} mt={6} width="40%" radius="xl" />
+                <Skeleton height={30} mt={6} width="70%" radius="xl" />
+                <Skeleton height={30} mt={6} width="90%" radius="xl" />
+                <Skeleton height={30} mt={6} width="60%" radius="xl" />
+                <Skeleton height={30} mt={6} width="30%" radius="xl" />
+                <Skeleton height={30} mt={6} width="70%" radius="xl" />
+                <Skeleton height={30} mt={6} width="80%" radius="xl" />
+                <Skeleton height={30} mt={6} width="80%" radius="xl" />
+                <Skeleton height={30} mt={6} width="40%" radius="xl" />
+                <Skeleton height={30} mt={6} width="50%" radius="xl" />
+                <Skeleton height={30} mt={6} width="77%" radius="xl" />
+              </Stack>
+            ) : (
+              <BlockNoteView editor={editor} theme={"light"} sideMenu={false}>
+                <div
+                  style={{
+                    position: "relative",
+                    display: "flex",
+                    zIndex: 1000,
+                  }}
+                >
+                  <SideMenuController />
+                </div>
+              </BlockNoteView>
+            )}
+          </div>
         </Container>
       </AppShell.Main>
     </AppShell>
